6017039aa3c46e34a76fd85c084496e091c04527
Victor Luca <vluca002@gmail.com>
2020-03-03 11:22:13 -0300

    digit dialog

---
+++ ConsultaINSS/app/src/main/java/com/example/consultainss/LastCardDigitDialogFragment.kt
@@ -0,0 +1,19 @@
+package com.example.consultainss
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LastCardDigitDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
